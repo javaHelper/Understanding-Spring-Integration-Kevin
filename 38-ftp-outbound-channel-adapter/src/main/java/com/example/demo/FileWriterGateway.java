83af27da6a5ca92cc09c47ed051bbc4cc9357f0e
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-12-09 16:19:15 +0530

    Add files via upload

---
+++ 38-ftp-outbound-channel-adapter/src/main/java/com/example/demo/FileWriterGateway.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+
+public interface FileWriterGateway {
+
+	public void write(@Header("fileName") String fileName, @Payload String message);
+}
